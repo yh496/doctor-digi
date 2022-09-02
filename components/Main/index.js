e9ba29aab653fed9833f72bc10bef97bf4f43b0a
June Kim <junekim@dhcp-vl2041-21505.eduroam.cornell.edu>
2022-09-02 17:50:07 -0400

    Implement record audio in main

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Styles from "./main.module.css";
+import RecordAudio from "../RecordAudio";
 
 const Main = () => {
-    return (
-        <div className={Styles.mainContainer}>
-                <video width="960px" height="540px" src="/idle.webm" autoPlay loop muted/>
-        </div>
-    )
-
-}
+  return (
+    <div className={Styles.mainContainer}>
+      <video
+        width="960px"
+        height="540px"
+        src="/idle.webm"
+        autoPlay
+        loop
+        muted
+      />
+      <RecordAudio />
+    </div>
+  );
+};
 
 export default Main;
