018d392fdb42a6575953e3c82e93cd8fef740e89
yh496 <yh496@cornell.edu>
2022-09-05 20:42:18 -0400

    more videos

--- components/Main/index.js
+++ components/Main/index.js
@@ -9,10 +9,9 @@ const Main = () => {
       <video
         width="960px"
         height="540px"
-        src="/digi_videos/idle_v1.webm"
+        src="/digi_videos/scenario1/depth1.webm"
         autoPlay
         loop
-        muted
       />
       <RecordAudio />
     </div>
