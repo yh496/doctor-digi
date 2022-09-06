eba714ff3815b308a22eb85710089cd7d0b6af3c
yh496 <yh496@cornell.edu>
2022-09-06 13:24:05 -0400

    add keywords

--- src/chatbot/speechKeywords.js
+++ src/chatbot/speechKeywords.js
@@ -2,7 +2,13 @@
 const speechToEventMap = {
     "appointment": "appointment",
     "yes": "yes",
-    "no": "no"
+    "no": "no",
+    "primary": "primary",
+    "physician": "physician", 
+    "dentist": "dentist",
+    "dermatologist": "dermatologist",
+    "psychiatrist": "psychiatrist", 
+    "phsyiatrist": "phsyiatrist"
 } 
 
 module.exports = speechToEventMap
