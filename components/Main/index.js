5efcc82162bf9620bd3e2d42fa161ed3d0630b6e
June Kim <junekim@Junes-MacBook-Pro.local>
2022-09-06 00:20:57 -0400

    Integrated video transition with STT

--- components/Main/index.js
+++ components/Main/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import Styles from "./main.module.css";
-import RecordAudio from "../RecordAudio";
-import NextResponse from "../NextResponse";
 
 import Start from "../Start";
 import Choices from "../Choices";
 
-import {getNextResponse} from "../../lib/ResponseGenerator";
+import { getNextResponse } from "../../lib/ResponseGenerator";
 
 import AudioStreamer from "../../lib/AudioHandler";
 
@@ -15,26 +13,29 @@ const Main = () => {
   const [startDigi, setStartDigi] = useState(false);
   const [recording, setRecording] = useState(false);
 
-  const [scenario, setScenario]= useState("start");
+  const [scenario, setScenario] = useState("start");
   const [depth, setDepth] = useState(0);
-  const videoRef = React.useRef();
-  const choicesRef = React.useRef();
+  const videoRef = useRef();
+  const choicesRef = useRef();
   let videoElement = videoRef.current;
   let choicesElement = choicesRef.current;
   const [videoUrl, setVideoUrl] = useState(
     "/digi_videos/starters/starter1.webm"
   );
-  const [sttResponse, setSttResponse] = useState("")
+  const [sttResponse, setSttResponse] = useState("");
 
   useEffect(() => {
     if (sttResponse) {
-      const {nextScenario, nextDepth, video} = getNextResponse({scenario, depth, response: sttResponse})
-      setScenario(nextScenario)
-      setDepth(nextDepth)
-      setVideoUrl(video)
+      const { nextScenario, nextDepth, video } = getNextResponse({
+        scenario,
+        depth,
+        response: sttResponse,
+      });
+      setScenario(nextScenario);
+      setDepth(nextDepth);
+      onVideoChange(video);
     }
-    
-  },[sttResponse])
+  }, [sttResponse]);
 
   useEffect(() => {
     videoElement = videoRef.current;
@@ -42,27 +43,41 @@ const Main = () => {
   });
 
   const onVideoChange = (url) => {
-    setVideoUrl(url);
+    if (videoElement) {
+      videoElement = videoRef.current;
+      videoElement.style = "";
+      choicesElement.style = "";
+      videoElement.style.opacity = 0;
+      videoElement.style.transitionDuration = 0;
+      choicesElement.style.opacity = 0;
+      choicesElement.style.transitionDuration = 0;
+      setVideoUrl(url);
+      setTimeout(() => {
+        videoElement.style.opacity = 1;
+        videoElement.style.transitionDuration = "1.5s";
+        choicesElement.style.opacity = 1;
+        choicesElement.style.transitionDuration = "1.5s";
+      }, 100);
+    }
   };
 
   const recordingCallback = (sttResponse) => {
-    console.log('sttResponse', sttResponse)
-    setSttResponse(sttResponse)
-  }
+    console.log("sttResponse", sttResponse);
+    setSttResponse(sttResponse);
+  };
 
   function startRecording() {
-		console.log("audio start recording...", scenario);
-		setRecording(true);
-		AudioStreamer.initRecording(recordingCallback, (error) => {
-			console.error("Error when recording", error);
-			setRecording(false);
-		});
-	}
+    console.log("audio start recording...", scenario);
+    setRecording(true);
+    AudioStreamer.initRecording(recordingCallback, (error) => {
+      console.error("Error when recording", error);
+      setRecording(false);
+    });
+  }
 
   const onVideoEnd = () => {
     startRecording();
-  }
-
+  };
 
   return (
     <div className={Styles.mainContainer}>
@@ -84,14 +99,6 @@ const Main = () => {
             ref={videoRef}
           />
           <Choices scenario={0} depth={0} choicesRef={choicesRef} />
-
-          <RecordAudio />
-          <NextResponse
-            videoRef={videoRef}
-            choicesRef={choicesRef}
-            src={videoUrl}
-            onVideoChange={onVideoChange}
-          />
         </div>
       )}
     </div>
